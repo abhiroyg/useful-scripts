e087e49477dc0b094ae54569e5e62922092ee88e
abhiroyg <abhilashroy.g@gmail.com>
2016-05-11 01:39:47 +0530

    init

---
+++ copy_to_harddisk.py
@@ -0,0 +1,6 @@
+# Read the filename where the filepaths/dirpaths that should be copied are present.
+
+# Read the dir/.. to which we have to copy
+
+# Merge if the directory is same
+# Replace if the file is same.
---
+++ get_files_modified_after.py
@@ -0,0 +1,16 @@
+# Get the directory/file we have to check for
+    # expand '.' to pwd
+    # if file check in pwd, if not present throw error
+# Parse the date given
+    # if can't parse, throw error
+
+# Using `date -r <filename/dirname>` to get the last modified date
+# either of the file or all the files in the directory.
+# If the directory has directories go into those directories and check.
+
+# Compare the last modified date with the date given in the above
+# loop.
+
+# Print the file names to a file.
+# If there is a directory in which all files are created/modified after
+# the given date, print only the directory name.
